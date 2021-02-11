ac75fe3111262c79e925b77d1c05ee2699e0ea16
Matt Werth <matthewwerth@gmail.com>
2021-02-11 06:34:46 -0500

    Initial SetUp

---
+++ src/main/java/com/matthewwerth/movieratingsapi/MovieRatingsApiApplication.java
@@ -0,0 +1,13 @@
+package com.matthewwerth.movieratingsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRatingsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRatingsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matthewwerth/movieratingsapi/MovieRatingsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matthewwerth.movieratingsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRatingsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
